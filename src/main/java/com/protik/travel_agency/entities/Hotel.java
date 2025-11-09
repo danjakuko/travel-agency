17b1467f3daa3904eb4da902f05f889a11561920
KelvinBanaj <kelvin.banaj06@gmail.com>
2025-11-09 10:42:54 +0100

    entities: airport,hotel static data : enum HotelStandard

--- src/main/java/com/protik/travel_agency/entities/Hotel.java
+++ src/main/java/com/protik/travel_agency/entities/Hotel.java
@@ -1,6 +1,9 @@
 package com.protik.travel_agency.entities;
 
+import com.protik.travel_agency.static_data.HotelStandard;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,5 +11,8 @@ import lombok.Data;
 @Table(name = "hotels")
 @Data
 public class Hotel extends GenericEntity {
-
+    private String name;
+    private String description;
+    @Enumerated(value = EnumType.STRING)
+    private HotelStandard standard;
 }
---
+++ src/main/java/com/protik/travel_agency/static_data/HotelStandard.java
@@ -0,0 +1,9 @@
+package com.protik.travel_agency.static_data;
+
+public enum HotelStandard {
+    ONE_STAR,
+    TWO_STAR,
+    THREE_STAR,
+    FOUR_STAR,
+    FIVE_STAR
+}
