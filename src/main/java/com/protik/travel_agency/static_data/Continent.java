5d70c562609869651e5e1f88dc1775cc80205a0f
Laura Zeqiri <laurazeqiri2007@gmail.com>
2025-11-09 10:41:10 +0100

    created enum for continents

---
+++ src/main/java/com/protik/travel_agency/static_data/Continent.java
@@ -0,0 +1,11 @@
+package com.protik.travel_agency.static_data;
+
+public enum Continent {
+    EUROPE,
+    ASIA,
+    SOUTH_AMERICA,
+    NORTH_AMERICA,
+    AUSTRALIA,
+    AFRICA,
+    ANTARCTICA,
+}
